c5f484459fa5af14e430a2bc68274eb1dad7c0d2
Piyush <piyushpl9527@gmail.com>
2025-03-29 03:27:45 +0530

    porduct-handler code added

--- handlers/product-handler.js
+++ handlers/product-handler.js
@@ -1,58 +1,107 @@
- const Product  = require("./../db/product");
+const Product = require("./../db/product");
 
- 
 //  ADD
- async function addProduct(model){
-    let product = new Product({
-         ...model
-    })
-    await product.save();
-    return product.toObject();
- }
+async function addProduct(model) {
+  let product = new Product({
+    ...model,
+  });
+  await product.save();
+  return product.toObject();
+}
 
-  // Update
+// Update
 
-  async function updateProduct(id,model) {
-    await Product.findByIdAndUpdate(id,model);
-  }
+async function updateProduct(id, model) {
+  await Product.findByIdAndUpdate(id, model);
+}
 
 //   Delete
 
-  async function deleteProduct(id) {
-    await Product.findByIdAndDelete(id);
-  }
-
-//     Get All Products 
-
-async function getAllProducts() {
-    let products = await Product.find()
-    return products.map((x)=>x.toObject());
+async function deleteProduct(id) {
+  await Product.findByIdAndDelete(id);
 }
 
+//     Get All Products
+
+async function getAllProducts() {
+  let products = await Product.find();
+  return products.map((x) => x.toObject());
+}
 
 //    Get  Particular  Product
 
 async function getProduct(id) {
-    let product = await Product.findById(id);
-    return product.toObject();
-};
-
+  let product = await Product.findById(id);
+  return product.toObject();
+}
 
 async function getNewProducts() {
-  let newProducts =  await Product.find({
-    isNewProduct:true,
+  let newProducts = await Product.find({
+    isNewProduct: true,
   });
-  return ( newProducts).map(x=>x.toObject());
+  return newProducts.map((x) => x.toObject());
 }
-
 
 async function getFeaturedProducts() {
-  let featuredProducts =  await Product.find({
-      isFeatured:true,
+  let featuredProducts = await Product.find({
+    isFeatured: true,
   });
-  return ( featuredProducts).map(x=>x.toObject());
+  return featuredProducts.map((x) => x.toObject());
 }
 
+async function getProductForListing(
+  searchTerm,
+  categoryId,
+  brandId,
+  page,
+  pageSize,
+  sortBy,
+  sortOrder
+) {
+  if (!sortBy) {
+    sortBy = "price";
+  }
 
-              
-module.exports ={addProduct, updateProduct,deleteProduct,getAllProducts,getProduct,getNewProducts,getFeaturedProducts, };
+  // ✅ Ensure sortOrder is either 1 or -1
+  if (sortOrder !== 1 && sortOrder !== -1) {
+    sortOrder = -1; // Default to descending
+  }
+
+  let queryFilter = {};
+  if (searchTerm) {
+    queryFilter.$or = [
+      { name: { $regex: ".*" + searchTerm + ".*", $options: "i" } }, // ✅ Case insensitive search
+      { shortDescription: { $regex: ".*" + searchTerm + ".*", $options: "i" } },
+    ];
+  }
+
+  if (categoryId) {
+    queryFilter.categoryId = {
+      $in: Array.isArray(categoryId) ? categoryId : [categoryId],
+    };
+  }
+
+  if (brandId) {
+    queryFilter.brandId = brandId;
+  }
+
+  console.log("Query Filter:", queryFilter); // ✅ Debugging log
+
+  const products = await Product.find(queryFilter)
+    .sort({ [sortBy]: +sortOrder }) // ✅ Correct sorting
+    .skip((+page - 1) * pageSize)
+    .limit(+pageSize);
+
+  return products.map((x) => x.toObject());
+}
+
+module.exports = {
+  addProduct,
+  updateProduct,
+  deleteProduct,
+  getAllProducts,
+  getProduct,
+  getNewProducts,
+  getFeaturedProducts,
+  getProductForListing,
+};
--- routes/customer.js
+++ routes/customer.js
@@ -1,18 +1,43 @@
 const express = require("express");
 const router = express.Router();
 const { getNewProducts } = require("../handlers/product-handler");
-const { getFeaturedProducts } = require("../handlers/product-handler")
+const { getFeaturedProducts } = require("../handlers/product-handler");
+const { getCategories } = require("../handlers/category-handler");
+const { getProductForListing } = require("../handlers/product-handler");
+const { getBrands } = require("../handlers/brand-handler");
 
-
-router.get("/new-products", async  (req,res)=> {
-    const products = await getNewProducts();
-        res.send(products);
-    
+router.get("/new-products", async (req, res) => {
+  const products = await getNewProducts();
+  res.send(products);
 });
 
-router.get("/featured-products", async  (req,res)=> {
-    const products = await getFeaturedProducts();
-        res.send(products);
+router.get("/featured-products", async (req, res) => {
+  const products = await getFeaturedProducts();
+  res.send(products);
 });
 
+router.get("/categories", async (req, res) => {
+  const categories = await getCategories();
+  res.send(categories);
+});
+
+router.get("/brands", async (req, res) => {
+  const brands = await getBrands();
+  res.send(brands);
+});
+
+router.get("/products", async (req, res) => {
+  const { searchTerm, categoryId, brandId, sortBy, sortOrder, page, pageSize } =
+    req.query;
+  const products = await getProductForListing(
+    searchTerm,
+    categoryId,
+    brandId,
+    sortBy,
+    sortOrder,
+    page,
+    pageSize
+  );
+  res.send(products);
+});
 module.exports = router;
